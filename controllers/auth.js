c4864f33e49ec3edb6f3c117d411f20d0c257223
Rostyslav Kachor <rostynia@gmail.com>
2023-04-21 18:18:18 +0300

    mentor for check hw-5

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,18 @@
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+
+const fs = require("fs/promises");
+const path = require("path");
 
 const { ctrlWrapper } = require("../utils");
 
 const { User } = require("../models/user");
 
 const { HttpError } = require("../helpers");
+
+const avatarDir = path.join(__dirname, "../", "public", "avatars")
+
 const {SECRET_KEY} = process.env;
 
 const register = async (req, res) => {
@@ -15,7 +22,10 @@ const register = async (req, res) => {
     throw HttpError(409, "Email already exist");
   }
   const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+
+
+  const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
 
   res.status(201).json({
     name: newUser.name,
@@ -65,9 +75,24 @@ const logout = async(req, res) => {
     })
 }
 
+const updateAvatar = async(req, res) => {
+    const {path: tempUpload, filename} = req.file;
+    const {_id} = req.user;
+    const savename = `${_id}_${filename}`
+    const resultUpload = path.join(avatarDir, savename);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("avatars", savename);
+    await User.findByIdAndUpdate(_id, {avatarURL});
+
+    res.json({
+        avatarURL,
+    })
+}
+
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
